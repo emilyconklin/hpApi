9d7556ec395cf6e9d7ede3d5e3a961d03dc3be67
Emily Conklin <Emilyconklin31@gmail.com>
2020-02-25 14:35:16 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ let fetchUrl = 'http://hp-api.herokuapp.com/api/characters';
 const cardText = document.querySelector('.container');
 const url = fetchUrl
 
-
+function capitalize(string) {
+ return string.charAt(0).toUpperCase() + string.slice(1)
+}
 
 fetch(url)
   .then(data => data.json())
   .then(data => {
+    
 
     let html = ``
 
@@ -21,12 +24,12 @@ fetch(url)
       </div>
       <div class="flip-card-back ${story.house}">
         <h2>${story.name}</h2>
-       <p>Species: ${story.species}</p>
-        <p>Gender: ${story.gender}</p>
+       <p>Species: ${capitalize(story.species)}</p>
+        <p>Gender: ${capitalize(story.gender)}</p>
         <p>House: ${story.house}</p>
-        <p>Patronus: ${story.patronus}</p>
-        <p>Wand: ${story.wand.wood}</P>
-         <P>Core: ${story.wand.core}</p>
+        <p>Patronus: ${capitalize(story.patronus)}</p>
+        <p>Wand: ${capitalize(story.wand.wood)}</P>
+         <P>Core: ${capitalize(story.wand.core)}</p>
       </div>
     </div>
   </div>
@@ -38,8 +41,4 @@ fetch(url)
     cardText.innerHTML = html
 
   })
-
-// console.log(data)
-
-// style="background-color:${color}"
 
