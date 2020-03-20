fee4a439d47cc63bc12c30a068fa87cccb58698e
Emily Conklin <emilyconklin31@gmail.com>
2020-03-19 18:49:57 -0600

    Search

--- app.js
+++ app.js
@@ -1,44 +1,57 @@
-let fetchUrl = 'https://hp-api.herokuapp.com/api/characters';
-const cardText = document.querySelector('.container');
-const url = fetchUrl
+const searchBar = document.getElementById('searchBar')
+const charactersList = document.querySelector('.container');
+let hpCharacters = [];
 
 function capitalize(string) {
- return string.charAt(0).toUpperCase() + string.slice(1)
+  return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
-fetch(url)
-  .then(data => data.json())
-  .then(data => {
-    
+searchBar.addEventListener('keyup', (e) => {
+      const searchString = e.target.value.toLowerCase();
+      const filteredCharacters = hpCharacters.filter((character) => {
+          return (character.name.toLowerCase().includes(searchString) || character.house.toLowerCase().includes(searchString)
+        );
+      })
+      displayCharacters(filteredCharacters);
+    });
 
-    let html = ``
 
-    data.forEach(function (story) {
+    const loadCharacters = async () => {
+      try {
+        const res = await fetch('https://hp-api.herokuapp.com/api/characters');
+        hpCharacters = await res.json();
+        displayCharacters(hpCharacters);
+      } catch (err) {
+        console.error(err);
+      }
+    };
 
-      html = html + `
-    <div class="flip-card">
-    <div class="flip-card-inner">
-      <div class="flip-card-front">
-      <img src="${story.image}" class="card-img">
-      <h2>${story.name}</h2>
-      </div>
-      <div class="flip-card-back ${story.house}">
-        <h2>${story.name}</h2>
-       <p>Species: ${capitalize(story.species)}</p>
-        <p>Gender: ${capitalize(story.gender)}</p>
-        <p>House: ${story.house}</p>
-        <p>Patronus: ${capitalize(story.patronus)}</p>
-        <p>Wand: ${capitalize(story.wand.wood)}</P>
-         <P>Core: ${capitalize(story.wand.core)}</p>
-      </div>
-    </div>
-  </div>
- 
-        `   
-    })
-    
+    const displayCharacters = (characters) => {
+      const htmlString = characters
+        .map((character) => {
+          return `
+            <div class="flip-card">
+            <div class="flip-card-inner">
+              <div class="flip-card-front">
+              <img src="${character.image}" class="card-img">
+              <h2>${character.name}</h2>
+              </div>
+              <div class="flip-card-back ${character.house}">
+                <h2>${character.name}</h2>
+               <p>Species: ${capitalize(character.species)}</p>
+                <p>Gender: ${capitalize(character.gender)}</p>
+                <p>House: ${character.house}</p>
+                <p>Patronus: ${capitalize(character.patronus)}</p>
+                <p>Wand: ${capitalize(character.wand.wood)}</P>
+                 <P>Core: ${capitalize(character.wand.core)}</p>
+              </div>
+            </div>
+          </div>
+         
+                `
+        })
+        .join('');
+      charactersList.innerHTML = htmlString;
+    };
 
-    cardText.innerHTML = html
-
-  })
-
+    loadCharacters();
